04fcbcd1f63e217d96f6e8950a18ff2d771cc6d3
vostok2609 <druzhinin260986@gmail.com>
2023-06-21 16:40:14 +0500

    3.0

--- game_v2.py
+++ game_v2.py
@@ -9,7 +9,7 @@ def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1
+        number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
         int: Число попыток
