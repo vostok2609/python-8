3251f8e044fe7837868b9698b4e9b9000035f5ce
vostok2609 <druzhinin260986@gmail.com>
2023-07-10 13:36:11 +0500

    v3.0

--- game_v2.py
+++ game_v2.py
@@ -19,18 +19,8 @@ def random_predict(number: int = 1) -> int:
     while True:
         count += 1
         predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number>predict_number:
-            predict_number = np.random.randint(predict_number, 101)
-        if number>predict_number:
-            predict_number = np.random.randint(predict_number, 101)
-        if number>predict_number:
-            predict_number = np.random.randint(predict_number, 101)    
-        if number<predict_number:   
-            predict_number = np.random.randint(1,predict_number)
-        if number<predict_number:   
-            predict_number = np.random.randint(1,predict_number)
-        if number<predict_number:   
-            predict_number = np.random.randint(1,predict_number)           
+        if number<predict_number:
+            predict_number = predict_number//2
         else:
             number == predict_number
             break  # выход из цикла если угадали
